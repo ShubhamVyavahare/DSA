ab17c82c234fdd28bad01dfe240fc9c362c47d71
Shubham Vyavahare <33980269+ShubhamVyavahare@users.noreply.github.com>
2022-06-12 11:44:17 +0530

    Implemented LinkedList and CircularLinkedList DS

---
+++ src/linkedlist/CircularLinkedList.java
@@ -0,0 +1,49 @@
+package linkedlist;
+
+public class CircularLinkedList {
+    Node head, tail;
+
+    private static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    void insertElements(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            tail.next = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+            tail.next = head;
+        }
+    }
+
+    void displayElements() {
+        Node n1 = head;
+
+        if (tail == null && head == null)
+            System.out.println("Circular Linked List is empty, nothing to display");
+        else {
+            do {
+                System.out.print(n1.data + " ");
+                n1 = n1.next;
+            } while (n1 != head);
+        }
+    }
+
+    void deleteElements() {
+        if (tail != head) {
+            head = head.next;
+            tail.next = head;
+        } else {
+            head = tail = null;
+        }
+    }
+}
---
+++ src/linkedlist/LinkedListSolution.java
@@ -0,0 +1,50 @@
+package linkedlist;
+
+import java.util.LinkedList;
+
+/**
+ * Create a LinkedList and implement it using concepts of inbuilt classes and methods
+ * Implement functions like:
+ * 1. Insert at beginning
+ * 2. Insert at end of linked list
+ * 3. Delete
+ * 4. Display the contents of linked list
+ * 5. Explore other methods - addAll, contains, size
+ * <p>
+ * Output :
+ * Original Linked List : []
+ * After adding elements --> Linked List : [3, 7, 10, 1, 4, 5]
+ * Adding 6 at 1st index --> Linked List : [3, 6, 7, 10, 1, 4, 5]
+ * Removing 2nd index --> Linked List : [3, 6, 10, 1, 4, 5]
+ * Linked list size : 6
+ * Is 4 contains in Linked List : true
+ */
+public class LinkedListSolution {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> l1 = new LinkedList<>();
+
+        System.out.println("Original Linked List : " + l1);
+
+        l1.add(3);
+        l1.add(7);
+        l1.add(10);
+        l1.add(1);
+        l1.add(4);
+        l1.add(5);
+
+        System.out.println("After adding elements --> Linked List : " + l1);
+
+        l1.add(1, 6);
+        System.out.println("Adding 6 at 1st index --> Linked List : " + l1);
+
+        l1.remove(2);
+        System.out.println("Removing 2nd index --> Linked List : " + l1);
+
+        int size = l1.size();
+        System.out.println("Linked list size : " + size);
+
+        boolean contains = l1.contains(4);
+        System.out.println("Is 4 contains in Linked List ? : " + contains);
+    }
+}
